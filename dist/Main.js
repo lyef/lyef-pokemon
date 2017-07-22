8a0a55253d45a4e21c4037a288da7b9f8de25229
Willian Justen <willianjustenqui@gmail.com>
2017-07-22 13:23:39 -0300

    Bump version 1.1.0

--- dist/Main.js
+++ dist/Main.js
@@ -6,16 +6,23 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 var Pokemon = function Pokemon(_ref) {
-    var name = _ref.name;
-    var gen = _ref.gen;
-    var label = _ref.label;
+    var name = _ref.name,
+        gen = _ref.gen,
+        label = _ref.label;
     return _react2['default'].createElement(
         'div',
         { className: 'pokemon' },
-        _react2['default'].createElement('img', {
+        gen === 'sun-moon' ? _react2['default'].createElement('img', {
+            src: 'https://www.pkparaiso.com/imagenes/sol-luna/sprites/animados/' + String(name) + '.gif',
+            alt: name
+        }) : _react2['default'].createElement('img', {
             src: 'http://www.pokestadium.com/sprites/' + String(gen) + '/' + String(name) + '.' + (gen === 'xy' ? 'gif' : 'png'),
             alt: name
         }),
@@ -28,9 +35,9 @@ var Pokemon = function Pokemon(_ref) {
 };
 
 Pokemon.propTypes = {
-    name: _react.PropTypes.string.isRequired,
-    gen: _react.PropTypes.string,
-    label: _react.PropTypes.bool
+    name: _propTypes2['default'].string.isRequired,
+    gen: _propTypes2['default'].string,
+    label: _propTypes2['default'].bool
 };
 
 Pokemon.defaultProps = {
