aca39240db9862488c0a7fdf332743749b3641ed
Willian Justen <willianjustenqui@gmail.com>
2017-07-22 13:16:22 -0300

    Update to use separated PropTypes

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Pokemon = ({ name, gen, label }) => (
     <div className="pokemon">
